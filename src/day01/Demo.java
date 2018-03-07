89df6f9da931af56db690592d6c7aded1c1fcd88
xiaoshang1314 <2254202368@qq.com>
2018-03-07 22:52:16 +0800

    first comit

---
+++ src/day01/Demo.java
@@ -0,0 +1,23 @@
+package day01;
+
+public class Demo {
+
+    /**
+     * main psvm
+     * @param args
+     */
+    public static void main(String[] args) {
+        int array[] = new int[]{5,6,7};
+        print(array);
+    }
+
+
+
+    public static void print(int[] array){
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+}
